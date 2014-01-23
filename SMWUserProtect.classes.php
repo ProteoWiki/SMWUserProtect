f9e6926836d366cffc6ee25e620ecddf3f68be0e
toniher <toniher@cau.cat>
2014-01-23 15:46:35 +0100

    adding some extensions

---
+++ SMWUserProtect.classes.php
@@ -0,0 +1,225 @@
+<?php
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo 'Not a valid entry point';
+	exit( 1 );
+	}
+
+if ( !defined( 'SMW_VERSION' ) ) {
+	echo 'This extension requires Semantic MediaWiki to be installed.';
+	exit( 1 );
+}
+
+
+/**
+ * This class handles the permissions of access to User depending on Props.
+ */
+class SMWUserProtect {
+
+	/**
+	 * UserCan function
+	 * @param $title Title
+	 * @param $user User
+	 * @param $action 
+	 * @param $result
+	 * @return boolean
+	 */
+	
+	public static function checkIfUserCan( &$title, &$user, $action, &$result ) {
+
+		global $wgSMWUserProtectGroups;
+		global $wgSMWUserProtectProps;
+		global $wgSMWUserProtectNS;
+		global $wgSMWUserProtectNSParent;
+	
+		// Process user	
+		$usergrps = $user->getEffectiveGroups();
+		$username = $user->getName();
+		
+		foreach ( $usergrps as $usergrp ) {
+		
+			if ( in_array( $usergrp, $wgSMWUserProtectGroups ) ) {
+				// If user is in allowed groups, we go ahead
+				return true;
+			}
+		}
+		
+		// Let's get Namespace
+		$ns = $title->getNamespace();
+		$titleText = $title->getText();
+		$fulltitleText = $title->getPrefixedText();
+	
+		//Check User
+		if ( $ns == NS_USER ) {
+			
+			if ( $username == $titleText ) {
+				
+				return true;
+			}
+			
+		}
+		
+		
+		// Check NS
+		foreach ( $wgSMWUserProtectNS as $NStarget ) {
+		
+			if ( $ns == $NStarget ) {
+		
+				// Case of page containing the property
+				foreach ( $wgSMWUserProtectProps as $prop ) {
+					self::getProperty( $fultitleText, $prop, $username );
+				}
+				
+				
+				// Case of page not containing the property -> but parents
+				if ( !in_array( $wgSMWUserProtectNSParent, $NStarget ) ) {
+				
+					// If not a parent page
+					// We get the parent
+					foreach ( $wgSMWUserProtectNSParent as $parent ) {
+					
+						$parentName = $wgContLang->getNsText( $parent );
+					
+						foreach ( $wgSMWUserProtectProps as $prop ) {
+		
+							$actualParent = SMWParent::getParent( $fulltitleText, $parentName );
+							
+							self::getProperty( $actualParent, $prop, $username );
+						}
+					
+					}
+				}
+				
+			}
+		}
+		
+	}
+	
+	/**
+	 * getProperty function
+	 * @param $titleText string
+	 * @param $prop string
+	 * @return boolean
+	 */
+	
+	private function getProperty( $titleText, $prop ) {
+			
+		$query_string = "[[".$titleText."]][[".$prop.":+]]";
+		
+		// Props to see
+		$props = array();
+		array_push( $props, $prop );
+		
+		// Get a list of usernames
+		$usernames = self::processResults( $query_string, $prop, false );
+		
+		if ( count( $usernames ) > 0 ) {
+			
+			if ( in_array( $usernames, $username ) ) {
+				
+				// If there -> we allow
+				return true;
+			}
+		}		
+		
+	}
+	
+	
+	/**
+	 * Function for processing query results
+	 * @param $query_string String : the query
+	 * @param $properties_to_display array(String): array of property names to display
+	 * @param $display_title Boolean : add the page title in the result
+	 * @return usernames array
+	 */	
+	
+	static function processResults ( $query_string, $properties_to_display, $display_title ) {
+		
+		// By default -> empty
+		$usernames = array();
+		
+		$results = self::getQueryResults( $query_string, $properties_to_display, $display_title );
+		
+		// In theory, there is only one row
+		while ( $row = $results->getNext() ) {
+		
+			$userContainer = $row[1];
+		
+			if ( !empty( $userContainer ) ) {
+		                    
+		                while ( $obj = $userContainer->getNextObject() ) {
+		                            
+					$pagevalue = $obj->getWikiValue();
+					
+					$userobj = User::newFromName( $pagevalue );
+					
+					
+					if ( $userobj && $userobj->getId() > 0 ) {
+						
+						array_push( $usernames, $userobj->getName() );
+						
+					}
+					
+		                }   
+		        } 
+		
+		}
+		
+		// Return list of usernames
+		return $usernames;
+		
+	}
+	
+	/**
+	 * This function returns to results of a certain query
+	 * Thank you Yaron Koren for advices concerning this code
+	 * @param $query_string String : the query
+	 * @param $properties_to_display array(String): array of property names to display
+	 * @param $display_title Boolean : add the page title in the result
+	 * @return TODO
+	 */
+	static function getQueryResults( $query_string, $properties_to_display, $display_title ) {
+		
+		// We use the Semantic MediaWiki Processor
+		// $smwgIP is defined by Semantic MediaWiki, and we don't allow
+		// this file to be sourced unless Semantic MediaWiki is included.
+		global $smwgIP;
+		include_once( $smwgIP . "/includes/SMW_QueryProcessor.php" );
+		
+		$params = array();
+		$inline = true;
+		$printlabel = "";
+		$printouts = array();
+		
+		// add the page name to the printouts
+		if ( $display_title ) {
+			$to_push = new SMWPrintRequest( SMWPrintRequest::PRINT_THIS, $printlabel );
+			array_push( $printouts, $to_push );
+		}
+		
+		// Push the properties to display in the printout array.
+		foreach ( $properties_to_display as $property ) {
+			if ( class_exists( 'SMWPropertyValue' ) ) { // SMW 1.4
+				$to_push = new SMWPrintRequest( SMWPrintRequest::PRINT_PROP, $printlabel, SMWPropertyValue::makeProperty( $property ) );
+			} else {
+				$to_push = new SMWPrintRequest( SMWPrintRequest::PRINT_PROP, $printlabel, Title::newFromText( $property, SMW_NS_PROPERTY ) );
+			}
+			array_push( $printouts, $to_push );
+		}
+		
+		if ( version_compare( SMW_VERSION, '1.6.1', '>' ) ) {
+			SMWQueryProcessor::addThisPrintout( $printouts, $params );
+			$params = SMWQueryProcessor::getProcessedParams( $params, $printouts );
+			$format = null;
+		}
+		else {
+			$format = 'auto';
+		}
+		
+		$query = SMWQueryProcessor::createQuery( $query_string, $params, $inline, $format, $printouts );
+		$results = smwfGetStore()->getQueryResult( $query );
+		
+		return $results;
+	}
+	
+}
---
+++ SMWUserProtect.php
@@ -0,0 +1,51 @@
+<?php
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo 'Not a valid entry point';
+	exit( 1 );
+}
+
+if ( !defined( 'SMW_VERSION' ) ) {
+	echo 'This extension requires Semantic MediaWiki to be installed.';
+	exit( 1 );
+}
+
+#
+# This is the path to your installation of SemanticTasks as
+# seen from the web. Change it if required ($wgScriptPath is the
+# path to the base directory of your wiki). No final slash.
+# #
+$spScriptPath = $wgScriptPath . '/extensions/SMWUserProtect';
+#
+
+# Extension credits
+$wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other'][] = array(
+	'path' => __FILE__,
+	'name' => 'SMWUserProtect',
+	'author' => array(
+		'[https://www.mediawiki.org/wiki/User:Toniher Toni Hermoso]'
+	),
+	'version' => '0.1',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:SMWUserProtect',
+	'descriptionmsg' => 'SMWUserProtect-desc',
+);
+
+
+// i18n
+$wgExtensionMessagesFiles['SMWUserProtect'] = dirname( __FILE__ ) . '/SMWUserProtect.i18n.php';
+
+// Autoloading
+$wgAutoloadClasses['SMWUserProtect'] = dirname( __FILE__ ) . '/SMWUserProtect.classes.php';
+
+// Hooks
+$wgHooks['userCan'][] = 'SMWUserProtect::checkIfUserCan';
+
+// Allowed groups 
+$wgSMWUserProtectGroups = array( 'sysop', 'team' );
+
+// User Property
+$wgSMWUserProtectProps = array( 'Has User' );
+
+// Namespaces with protection
+$wgSMWUserProtectNS = array( NS_REQUEST, NS_SAMPLE, NS_PROCESS );
+$wgSMWUserProtectNSParent = array( NS_REQUEST );
