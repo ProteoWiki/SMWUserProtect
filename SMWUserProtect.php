8690677b31bf8c3ed5c5e5d9d9a867302a8c0644
toniher <toniher@cau.cat>
2015-06-17 12:36:16 +0200

    Update user Protect

--- SMWUserProtect.php
+++ SMWUserProtect.php
@@ -10,53 +10,49 @@ if ( !defined( 'SMW_VERSION' ) ) {
 	exit( 1 );
 }
 
-#
-# This is the path to your installation of SemanticTasks as
-# seen from the web. Change it if required ($wgScriptPath is the
-# path to the base directory of your wiki). No final slash.
-# #
-$spScriptPath = $wgScriptPath . '/extensions/SMWUserProtect';
-#
 
-# Extension credits
-$wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other'][] = array(
-	'path' => __FILE__,
-	'name' => 'SMWUserProtect',
-	'author' => array(
-		'[https://www.mediawiki.org/wiki/User:Toniher Toni Hermoso]'
-	),
-	'version' => '0.1',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:SMWUserProtect',
-	'descriptionmsg' => 'SMWUserProtect-desc',
-);
+call_user_func( function () {
 
+	# Extension credits
+	$GLOBALS['wgExtensionCredits'][defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other'][] = array(
+		'path' => __FILE__,
+		'name' => 'SMWUserProtect',
+		'author' => array(
+			'[https://www.mediawiki.org/wiki/User:Toniher Toni Hermoso]'
+		),
+		'version' => '0.1',
+		'url' => 'https://www.mediawiki.org/wiki/Extension:SMWUserProtect',
+		'descriptionmsg' => 'SMWUserProtect-desc',
+	);
+	
+	
+	// i18n
+	$GLOBALS['$wgExtensionMessagesFiles']['SMWUserProtect'] = dirname( __FILE__ ) . '/SMWUserProtect.i18n.php';
+	
+	// Autoloading
+	$GLOBALS['wgAutoloadClasses']['SMWUserProtect'] = dirname( __FILE__ ) . '/SMWUserProtect.classes.php';
+	
+	// Allowed groups 
+	$GLOBALS['wgSMWUserProtectGroups'] = array( 'sysop', 'team' );
+	
+	// User Property
+	$GLOBALS['wgSMWUserProtectProps'] = array( 'Has User' );
+	
+	// Namespaces with protection
+	$GLOBALS['wgSMWUserProtectNS'] = array( NS_REQUEST, NS_SAMPLE, NS_PROCESS );
+	$GLOBALS['wgSMWUserProtectNSParent'] = array( NS_REQUEST );
+	
+	# Edit prohibited depending on the value
+	$GLOBALS['wgSMWUserProtectEditClose'] = array (
+		NS_REQUEST => array(
+			"Has Request Status" => array( "Accepted", "Closed", "Discarded" )
+		)
+	);
+	
+	# If visiting user is not in the groups above, avoid reading User pages
+	$GLOBALS['wgSMWUserProtectUserPages'] = true;
 
-// i18n
-$wgExtensionMessagesFiles['SMWUserProtect'] = dirname( __FILE__ ) . '/SMWUserProtect.i18n.php';
+	// Hooks
+	$GLOBALS['wgHooks']['userCan'][] = 'SMWUserProtect::checkIfUserCan';
 
-// Autoloading
-$wgAutoloadClasses['SMWUserProtect'] = dirname( __FILE__ ) . '/SMWUserProtect.classes.php';
-
-// Hooks
-$wgHooks['userCan'][] = 'SMWUserProtect::checkIfUserCan';
-
-// Allowed groups 
-$wgSMWUserProtectGroups = array( 'sysop', 'team' );
-
-// User Property
-$wgSMWUserProtectProps = array( 'Has User' );
-
-// Namespaces with protection
-$wgSMWUserProtectNS = array( NS_REQUEST, NS_SAMPLE, NS_PROCESS );
-$wgSMWUserProtectNSParent = array( NS_REQUEST );
-
-# Edit prohibited depending on the value
-$wgSMWUserProtectEditClose = array (
-	NS_REQUEST => array(
-		"Has Request Status" => array( "Accepted", "Closed", "Discarded" )
-	)
-);
-
-# If visiting user is not in the groups above, avoid reading User pages
-$wgSMWUserProtectUserPages = true; 
-
+});
